f55c3ebae0456cf6b3343fea3dac0e67c3324b07
Stanislav Kutasevits <stan@bellcom.dk>
2018-02-06 16:13:00 +0200

    Adding files to os2dagsorden profile

---
+++ README.md
@@ -0,0 +1,2 @@
+The copy of os2dagsorden repository (os2dagsorden profile only) from this release: https://github.com/bellcom/os2dagsorden-2.0/tree/7.x-2.6.9
+commit: e07e73b5693baab9c9994c1bd75627211cb86559
---
+++ themes/os2dagsorden_theme/templates/html.tpl.php
@@ -0,0 +1,27 @@
+<?php print $doctype; ?>
+<html lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"<?php print $rdf->version . $rdf->namespaces; ?>>
+<head<?php print $rdf->profile; ?>>
+  <?php print $head; ?>
+  <title><?php print $head_title; ?></title>
+  <link rel="apple-touch-icon" href="/sites/default/files/icons/apple-touch-icon.png" />
+  <link rel="apple-touch-icon" sizes="57x57" href="/sites/default/files/icons/apple-touch-icon-57x57.png" />
+  <link rel="apple-touch-icon" sizes="72x72" href="/sites/default/files/icons/apple-touch-icon-72x72.png" />
+  <link rel="apple-touch-icon" sizes="76x76" href="/sites/default/files/icons/apple-touch-icon-76x76.png" />
+  <link rel="apple-touch-icon" sizes="114x114" href="/sites/default/files/icons/apple-touch-icon-114x114.png" />
+  <link rel="apple-touch-icon" sizes="120x120" href="/sites/default/files/icons/apple-touch-icon-120x120.png" />
+  <link rel="apple-touch-icon" sizes="144x144" href="/sites/default/files/icons/apple-touch-icon-144x144.png" />
+  <link rel="apple-touch-icon" sizes="152x152" href="/sites/default/files/icons/apple-touch-icon-152x152.png" />
+  <link rel="apple-touch-icon" sizes="180x180" href="/sites/default/files/icons/apple-touch-icon-180x180.png" />
+  <?php print $styles; ?>
+  <?php print $scripts; ?>
+  <!--[if lt IE 9]><script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+</head>
+<body<?php print $attributes;?>>
+  <div id="skip-link">
+    <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
+  </div>
+  <?php print $page_top; ?>
+  <?php print $page; ?>
+  <?php print $page_bottom; ?>
+</body>
+</html>
