40a6a1a892ba81101f81424a947dcae523ce957e
Stanislav Kutasevits <stan@bellcom.dk>
2018-10-25 13:59:46 +0300

    SVENDK-736 auto bp number filling; ajax delete fixes

--- modules/os2dagsorden/os2dagsorden_create_agenda/js/form_js.js
+++ modules/os2dagsorden/os2dagsorden_create_agenda/js/form_js.js
@@ -1,16 +1,19 @@
 (function ($) {
     Drupal.behaviors.confirm = {
         attach: function (context, settings) {
-            var events = $('.form-submit-delete').clone(true).data('events');// Get the jQuery events.
-            $('.form-submit-delete').unbind('mousedown'); // Remove the click events.
-            $('.form-submit-delete').mousedown(function () {
-                if (confirm(Drupal.t('Are you sure you want to delete this item?'))) {
-                    $.each(events.mousedown, function () {
-                        this.handler(); // Invoke the mousedown handlers that was removed.
-                    });
-                }
-                // Prevent default action.
-                return false;
+            $('.form-submit-delete').once('form-submit-delete-processed').each(function(index) {
+                var events = $(this).clone(true).data('events');// Get the jQuery events.
+                $(this).unbind('mousedown'); // Remove the click events.
+                $(this).mousedown(function() {
+                    if (confirm(Drupal.t('Are you sure you want to delete this item?'))) {
+                        $.each(events.mousedown, function () {
+                            this.handler(); // Invoke the mousedown handlers that was removed.
+                        });
+                    }
+
+                    // Prevent default action.
+                    return false;
+                })
             });
         }
     }
