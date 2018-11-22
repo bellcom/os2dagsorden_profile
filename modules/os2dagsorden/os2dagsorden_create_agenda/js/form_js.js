734cc467098610624b97b8313b1b90a12e7d80a6
Juulia Devadze <jd@est2>
2018-11-22 19:47:22 +0100

    SVENDK-756 Multiple bilags upload

--- modules/os2dagsorden/os2dagsorden_create_agenda/js/form_js.js
+++ modules/os2dagsorden/os2dagsorden_create_agenda/js/form_js.js
@@ -1,37 +1,47 @@
 (function ($) {
-    Drupal.behaviors.confirm = {
-        attach: function (context, settings) {
-            $('.create-agenda-edit-delete-container.form-submit-delete').once('form-submit-delete-processed').each(function(index) {
-                var events = $(this).clone(true).data('events');// Get the jQuery events.
-                $(this).unbind('mousedown'); // Remove the click events.
-                $(this).mousedown(function() {
-                    if (confirm(Drupal.t('Are you sure you want to delete this item?'))) {
-                        $.each(events.mousedown, function () {
-                            this.handler(); // Invoke the mousedown handlers that was removed.
-                        });
-                    }
-
-                    // Prevent default action.
-                    return false;
-                })
+  Drupal.behaviors.confirm = {
+    attach: function (context, settings) {
+      $('.create-agenda-edit-delete-container.form-submit-delete').once('form-submit-delete-processed').each(function (index) {
+        var events = $(this).clone(true).data('events');// Get the jQuery events.
+        $(this).unbind('mousedown'); // Remove the click events.
+        $(this).mousedown(function () {
+          if (confirm(Drupal.t('Are you sure you want to delete this item?'))) {
+            $.each(events.mousedown, function () {
+              this.handler(); // Invoke the mousedown handlers that was removed.
             });
-             jQuery('.edit-meeting-btn.form-submit-delete').click(function(event){
-              event.preventDefault();
-              if (confirm(Drupal.t('Are you sure you want to delete this item?'))) {
-                var element = jQuery(this);
-                jQuery.get(jQuery(this).attr('href'), function(data) {
-                  if (data.status  == 'OK') {
-                    if (data.element == 'bp') {
-                      element.parents('tr').remove();
-                    }
-                    else if (data.element == 'bpa') {
-                      element.parents('li').remove();
-                    }
-                  }
+          }
 
-                });
+          // Prevent default action.
+          return false;
+        })
+      });
+      jQuery('.edit-meeting-btn.form-submit-delete').click(function (event) {
+        event.preventDefault();
+        if (confirm(Drupal.t('Are you sure you want to delete this item?'))) {
+          var element = jQuery(this);
+          jQuery.get(jQuery(this).attr('href'), function (data) {
+            if (data.status == 'OK') {
+              if (data.element == 'bp') {
+                element.parents('tr').remove();
+              } else if (data.element == 'bpa') {
+                element.parents('li').remove();
               }
-            });
+            }
+
+          });
         }
+      });
+      $("#js-bpas-fs .fieldset-wrapper").sortable({
+        update: function (event, ui) {
+          $.post(
+            '/agenda/meeting/bullet-point/sort',
+            {
+              bullet_point: $("#js-bpas-fs").data("bp_id"),
+              attachments: $('#js-bpas-fs .fieldset-wrapper').sortable('serialize')
+            }
+          );
+        }
+      });
     }
+  }
 })(jQuery);
