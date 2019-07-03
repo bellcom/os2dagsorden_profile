877a37c5b9b357fb24abfe3030e036e0915e1074
Morten Nissen <mail@hjemmesidekongen.dk>
2019-07-03 13:39:28 +0200

    Responsive

--- themes/os2dagsorden_theme/js/app.js
+++ themes/os2dagsorden_theme/js/app.js
@@ -16,4 +16,19 @@ jQuery(function($) {
   var $toggleSidrButton = $('<button />').addClass('responsive-button responsive-button--sidr-toggle');
 
   //$page.prepend($responsiveHeader);
+
+  // Force open the bulletpoint attachments by clicking
+  // the "toggle all" on mobile. The button is visually
+  // hidden on mobile by CSS, but is visible on
+  // desktop.
+  if (window.outerWidth < 767) {
+
+    if (document.body.classList.contains('page-meeting')) {
+      const toggleAllButton = document.querySelector('.hide_show_all_attachments_text.top');
+
+      if (! toggleAllButton.classList.contains('opened')) {
+        toggleAllButton.click();
+      }
+    }
+  }
 });
