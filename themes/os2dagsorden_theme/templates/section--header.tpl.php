2803eca46627e5481d048d537ffcf5ec7e1bb8e2
Morten Nissen <mail@hjemmesidekongen.dk>
2019-07-04 11:27:06 +0200

    Responsive logo

--- themes/os2dagsorden_theme/templates/section--header.tpl.php
+++ themes/os2dagsorden_theme/templates/section--header.tpl.php
@@ -1,13 +1,30 @@
+<?php
+  $current_theme = variable_get('theme_default', 'none');
+  $path = base_path() . drupal_get_path('theme', $current_theme);
+?>
+
 <header<?php print $attributes; ?>>
   <?php print $content; ?>
 </header>
 
+<!-- Begin - responsive header -->
 <div class="responsive-header">
-  <a href="/meetings-search" class="responsive-button responsive-button--search">
-    <i class="material-icons">search</i>
-  </a>
+  <div class="flexy-row">
+    <a href="/">
+      <img src="<?=$path; ?>/img/logo.png" alt="OS2dagsorden"/>
+    </a>
+    
+    <div class="flexy-spacer"></div>
 
-  <button class="responsive-button responsive-button--sidr-toggle">
-    <i class="material-icons">menu</i>
-  </button>
+    <div>
+      <a href="/meetings-search" class="responsive-button responsive-button--search">
+        <i class="material-icons">search</i>
+      </a>
+
+      <button class="responsive-button responsive-button--sidr-toggle">
+        <i class="material-icons">menu</i>
+      </button>
+    </div>
+  </div>
 </div>
+<!-- End - responsive header -->
