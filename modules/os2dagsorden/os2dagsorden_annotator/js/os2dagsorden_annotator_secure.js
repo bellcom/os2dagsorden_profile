1accf7a03e61a7545c831023d5383d0c845e2a42
Juulia Devadze <jd@svk-dags01>
2019-12-17 17:24:08 +0100

    SVENDK-832 access to image files

--- modules/os2dagsorden/os2dagsorden_annotator/js/os2dagsorden_annotator_secure.js
+++ modules/os2dagsorden/os2dagsorden_annotator/js/os2dagsorden_annotator_secure.js
@@ -1,34 +1,6 @@
 function add_annotator(meeting_id, bullet_point_id, bilag_id, element_to_annotate, url, filter) {
   jQuery(document).ready(function() {
-		/*jQuery(element_to_annotate).annotator().annotator('addPlugin', 'Touch', {
-			force: 1,
-			useHighlighter: location.search.indexOf('highlighter') > -1
-		});
-		if (filter){
-		  jQuery(element_to_annotate).annotator().annotator('addPlugin', 'Filter');
-		}
 
-		jQuery(element_to_annotate).annotator().annotator('addPlugin', 'Store', {
-			// The endpoint of the store on your server.
-			prefix: url,
-			annotationData: {
-				'bilag_id': bilag_id,
-				'bullet_point_id': bullet_point_id,
-				'meeting_id': meeting_id,
-			},
-			loadFromSearch: {
-				'bilag_id': bilag_id,
-				'bullet_point_id': bullet_point_id,
-				'meeting_id': meeting_id,
-			},
-			urls: {
-			  create:  'annotator/create',
-			  read:    'annotator/read/:id',
-			  update:  'annotator/update/:id',
-			  destroy: 'annotator/delete/:id',
-			  search:  'annotator/search'
-			}
-		});*/
     var pageUri = function () {
     return {
         beforeAnnotationCreated: function (ann) {
@@ -62,44 +34,7 @@ function add_annotator(meeting_id, bullet_point_id, bilag_id, element_to_annotat
 				 meeting_id : meeting_id,
        });
    });
-   
 
-        if (!jQuery("body .annotator-touch-controls.dummy-controls").length ) {
-            jQuery('body').append(
-                '<div class="annotator-touch-widget annotator-touch-controls dummy-controls">' +
-                    '<div class="annotator-touch-widget-inner">' +
-                        '<a class="annotator-button annotator-add annotator-focus">' + Drupal.settings.os2dagsorden_annotator.annotator_button_text + '</a>' +
-                    '</div>' +
-                '</div>'
-            );
-            jQuery('body .annotator-touch-controls.dummy-controls').click(function(e) {
-                e.preventDefault();
-                if (jQuery("#ToolTipDiv2").css('display') != 'block') {
-                  jQuery("#ToolTipDiv2").html("Marker tekst og klik herefter på Lav note").fadeIn(400);
-                  setTimeout(function(){
-                      jQuery("#ToolTipDiv2").fadeOut("slow");
-                  },5000)
-                }
-                else {
-                  jQuery("#ToolTipDiv2").css({'display': 'none'});
-                }
-            });
-        }
-
-        // Changing button text.
-        jQuery(".annotator-button.annotator-add.annotator-focus").text(Drupal.settings.os2dagsorden_annotator.annotator_button_text);
-        if (Drupal.settings.os2dagsorden_annotator.annotator_hide_text) {
-            jQuery(".annotator-button.annotator-add.annotator-focus").html('&nbsp;');
-            jQuery(".annotator-button.annotator-add.annotator-focus").css("cssText", "padding-right: 0 !important;");
-        }
-        if (Drupal.settings.os2dagsorden_annotator.annotator_use_title) {
-            jQuery(".annotator-touch-widget-inner").attr("title", Drupal.settings.os2dagsorden_annotator.annotator_button_text);
-        }
-        if (jQuery(".bilag-list-btn").length) {
-          jQuery(".bilag-list-btn").click(function(){
-            jQuery(".bilag-list").toggle();
-          });
-        }
   });
   jQuery(window).load(function() {
     // Scroll bar on attachment page.
