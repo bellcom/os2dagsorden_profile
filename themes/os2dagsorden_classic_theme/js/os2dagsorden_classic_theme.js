7e22c184cc7d78e099c5ee02ee4e1d3b0ebe2004
Juulia Devadze <jd@svk-dags01>
2019-12-18 19:30:16 +0100

    SVENDK-832 annotation display fix

--- themes/os2dagsorden_classic_theme/js/os2dagsorden_classic_theme.js
+++ themes/os2dagsorden_classic_theme/js/os2dagsorden_classic_theme.js
@@ -710,7 +710,7 @@ function attachment_load_content(bulletPointIndex, index_attachment, url){
 	jQuery("#attachment_text_container_"+bulletPointIndex+"_"+index_attachment).children().first().append(html);
 
 	//add annotator to it
-	add_annotator(meeting_id, bullet_point_id, bilag_id, "attachment_text_container_"+bulletPointIndex+"_"+index_attachment ,url, false);
+	add_annotator(meeting_id, bullet_point_id, bilag_id, "bpa_"+ meeting_id + "_" + bullet_point_id + "_" + bilag_id ,url, false);
 
 	//add preview stamp picture, if is actual bilag
 	if (jQuery(".bpa-" + meeting_id + "-" + bullet_point_id + "-" + bilag_id).hasClass("attachment_text_trimmed_container"))
