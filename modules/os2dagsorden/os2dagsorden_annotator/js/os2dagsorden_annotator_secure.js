9db46dfa662a5cc609b90227168e56c4d0bc9d21
Juulia Devadze <jd@svk-dags01>
2020-03-17 15:56:55 +0100

    SVENDK-848 Jump to note

--- modules/os2dagsorden/os2dagsorden_annotator/js/os2dagsorden_annotator_secure.js
+++ modules/os2dagsorden/os2dagsorden_annotator/js/os2dagsorden_annotator_secure.js
@@ -1,6 +1,6 @@
 function add_annotator(meeting_id, bullet_point_id, bilag_id, element_to_annotate, url, filter) {
   jQuery(document).ready(function() {
-
+    var hash = window.location.hash.substring(1);
     var pageUri = function () {
     return {
         beforeAnnotationCreated: function (ann) {
@@ -8,8 +8,15 @@ function add_annotator(meeting_id, bullet_point_id, bilag_id, element_to_annotat
               ann.bullet_point_id= bullet_point_id;
               ann.meeting_id= meeting_id;
         },
-    };
-};
+        annotationsLoaded: function (ann) {
+          if (hash && !(jQuery(".annotator-outline-pager").length && Drupal.settings.os2dagsorden_annotator.attachment_add_scrollbar)){
+            jQuery("body, html").animate({
+              scrollTop:  jQuery('span[data-annotation-id='+ hash +']').offset().top
+            }, 100 );
+          }
+        }
+      }
+    }
     var app = new annotator.App();
     app.include(annotator.touch.main, {element: document.getElementById(element_to_annotate)})
      .include(annotator.storage.http, {
@@ -42,6 +49,7 @@ function add_annotator(meeting_id, bullet_point_id, bilag_id, element_to_annotat
         }
   });
   jQuery(window).load(function() {
+    var hash = window.location.hash.substring(1);
     // Scroll bar on attachment page.
         if (jQuery(".annotator-outline-pager").length && Drupal.settings.os2dagsorden_annotator.attachment_add_scrollbar) {
           if (Drupal.settings.os2dagsorden_annotator.attachment_add_scrollbar == 'attachment_add_scrollbar_both') {
@@ -51,6 +59,7 @@ function add_annotator(meeting_id, bullet_point_id, bilag_id, element_to_annotat
             var height = jQuery(window).height();
             jQuery('#content.has-outline-pager').innerHeight(height);
             jQuery(".annotator-outline-pager").innerHeight(height);
+
           }
           else if (Drupal.settings.os2dagsorden_annotator.attachment_add_scrollbar == 'attachment_add_scrollbar_attachment') {
           var pager_height = jQuery(".annotator-outline-pager").outerHeight();
@@ -58,6 +67,7 @@ function add_annotator(meeting_id, bullet_point_id, bilag_id, element_to_annotat
           var height = (pager_height > window_height)? pager_height : window_height;
           jQuery('#content.has-outline-pager').innerHeight(height);
           jQuery("#content.has-outline-pager").css('overflow-y', 'auto');
+
         }
         else if (Drupal.settings.os2dagsorden_annotator.attachment_add_scrollbar == 'attachment_add_scrollbar_thumbnails') {
           var attachment_height = jQuery("#content.has-outline-pager").outerHeight();
@@ -66,6 +76,11 @@ function add_annotator(meeting_id, bullet_point_id, bilag_id, element_to_annotat
           jQuery('.annotator-outline-pager').innerHeight(height);
           jQuery(".annotator-outline-pager").css('overflow-y', 'auto');
         }
+        var offset = jQuery('span[data-annotation-id='+ hash +']').offset().top - jQuery('#content.has-outline-pager').offset().top - 40;
+        jQuery("#content").animate({
+          scrollTop:  offset
+        }, 100 );
+        console.log(offset);
       }
   });
 }
